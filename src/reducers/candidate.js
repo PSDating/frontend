3529ff6d14392161df7f7217b363e0577c5b956c
Rodrigo García <rodrigo.garcia.leon@ing.nl>
2018-03-21 21:26:09 +0100

    add candidate reducer

--- src/reducers/candidate.js
+++ src/reducers/candidate.js
@@ -1,10 +1,9 @@
 const user = (state = null, action) => {
   if (action.type === "LOGIN_SUCCESSFUL") {
-    action.user.loggedIn = true;
     return action.user;
   }
 
   return state;
 };
 
-export default candidate;
+export default user;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import user from "./user.js";
 import message from "./message.js";
 import preferences from "./preferences.js";
+import candidate from "./candidate.js";
 
 const PSDating = combineReducers({
   user,
   message,
-  preferences
+  preferences,
+  candidate
 });
 
 export default PSDating;
