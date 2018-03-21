f2983ce841dc6b5d95c37e79e4dbd8f56b849074
Rodrigo García <rodrigo.garcia.leon@ing.nl>
2018-03-21 20:25:43 +0100

    add preferences list

--- src/components/Preferences.jsx
+++ src/components/Preferences.jsx
@@ -1,8 +1,43 @@
 import React from "react";
-import { Container, Label } from "semantic-ui-react";
+import { Image, Item } from 'semantic-ui-react';
 
-const Preferences = () => (
-  <Label>Preferences</Label>
-)
+const categories = [
+  {
+    id: 1,
+    label: "cat1",
+    image: "https://react.semantic-ui.com/assets/images/wireframe/image.png"
+  },
+  {
+    id: 2,
+    label: "cat2",
+    image: "https://react.semantic-ui.com/assets/images/wireframe/image.png"
+  },
+  {
+    id: 3,
+    label: "cat3",
+    image: "https://react.semantic-ui.com/assets/images/wireframe/image.png"
+  }
+];
+
+const Preference = ({ label, image }) => {
+  return (
+    <Item>
+      <Item.Image size='tiny' src={image} />
+      <Item.Content>
+        {label}
+      </Item.Content>
+    </Item>
+  )
+}
+
+const Preferences = () => {
+  return (
+    <Item.Group>
+      {
+        categories.map(category => <Preference label={category.label} image={category.image} />)
+      }
+    </Item.Group>
+  )
+}
 
 export default Preferences;
