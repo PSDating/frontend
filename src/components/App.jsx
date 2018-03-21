d3c384525b3bf72c25bb5524a044564f62e5f4df
René Bulsing <rene.bulsing@ing.nl>
2018-03-21 22:47:57 +0100

    Created an empty Matches page and made the menu a bit more fancy.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Provider } from "react-redux";
-import { Container, Header, Divider } from "semantic-ui-react";
+import { Menu, Container, Header, Divider } from "semantic-ui-react";
 import {
   BrowserRouter as Router,
   Route,
-  Link,
-  Redirect,
-  withRouter
+  Link
 } from "react-router-dom";
 
 import MessageContainer from "../containers/MessageContainer.js";
+import MatchesContainer from "../containers/MatchesContainer.js";
 import UserContainer from "../containers/UserContainer.js";
 import Selection from "../components/Selection.jsx";
 import RegistrationContainer from "../containers/RegistrationContainer.js";
@@ -22,14 +21,15 @@ const App = ({ store }) => (
     <Router>
       <Container>
         <Header as="h1">PSDating</Header>
-        <ul>
-          <li><Link to="/message">Message</Link></li>
-          <li><Link to="/user">User</Link></li>
-          <li><Link to="/selection">Selection</Link></li>
-          <li><Link to="/register">Register</Link></li>
-        </ul>
-        <Divider />
+        <Menu>
+          <Menu.Item as={Link} to="/message">Message</Menu.Item>
+          <Menu.Item as={Link} to="/matches">Matches</Menu.Item>
+          <Menu.Item as={Link} to="/user">User</Menu.Item>
+          <Menu.Item as={Link} to="/selection">Selection</Menu.Item>
+          <Menu.Item as={Link} to="/register">Register</Menu.Item>
+        </Menu>
         <Route path="/message" component={MessageContainer} />
+        <Route path="/matches" component={MatchesContainer} />
         <Route path="/user" component={UserContainer} />
         <Route path="/selection" component={Selection} />
         <Route path="/register" component={RegistrationContainer} />
