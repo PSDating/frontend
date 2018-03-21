9e801c31c01fd08b2c8c2fe0a666897a9fa18f26
René Bulsing <rene.bulsing@ing.nl>
2018-03-21 19:13:41 +0100

    Adding routing to the top level App component. Also added some examples for routing and path protection.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,33 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Provider } from "react-redux";
 import { Container, Header, Divider } from "semantic-ui-react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from "react-router-dom";
+
 import MessageContainer from "../containers/MessageContainer.js";
+import UserContainer from "../containers/UserContainer.js";
 import "semantic-ui-css/semantic.min.css";
 import "../../css/psdating.css";
 
 const App = ({ store }) => (
   <Provider store={store}>
-    <Container>
-      <Header as="h1">PSDating</Header>
-      <Divider />
-      <MessageContainer />
-    </Container>
+    <Router>
+      <Container>
+        <Header as="h1">PSDating</Header>
+        <ul>
+          <li><Link to="/message">Message</Link></li>
+          <li><Link to="/user">User</Link></li>
+        </ul>
+        <Divider />
+        <Route path="/message" component={MessageContainer} />
+        <Route path="/user" component={UserContainer} />
+      </Container>
+    </Router>
   </Provider>
 );
 
