eadd25833cd6e0562d15f967dc39ecc52800a095
Rodrigo García <rodrigo.garcia.leon@ing.nl>
2018-03-22 01:56:44 +0100

    implement redirect on scanner

--- src/components/Scanner.jsx
+++ src/components/Scanner.jsx
@@ -1,14 +1,37 @@
 import React from "react";
 import { Container, Header, Input, Button, Image, Label } from "semantic-ui-react";
+import { Redirect } from "react-router-dom";
+class Scanner extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { authorized: false};
+  }
 
-const Scanner = ({ image }) => (
-  <Container>
-    <Header as="h2">Scan</Header>
-    <Image className="scanner-image" src={image}></Image>
-    <Label>Enter password</Label>
-    <Input placeholder='Search...' type="password" />
-    <Button>Authorize</Button>
-  </Container>
-)
+  render() {
+    if (this.state.authorized) {
+      return (
+        <Redirect
+          to={{
+            pathname: "/selection"
+          }}
+        />
+      )
+    }
+
+    return (
+      <Container>
+        <Header as="h2">Scan</Header>
+        <Image className="scanner-image" src={this.props.image}></Image>
+        <Label>Enter password</Label>
+        <Input placeholder='Search...' type="password" />
+        <Button onClick={e => {
+          e.preventDefault();
+          this.setState({ authorized: true })
+        }}>Authorize</Button>
+      </Container>
+    )
+  }
+
+}
 
 export default Scanner;
