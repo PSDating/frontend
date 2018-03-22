fc295c7e0564ed2fa5a94ba9cd8f2b113fcfa4c4
Rodrigo García <rodrigo.garcia.leon@ing.nl>
2018-03-22 03:17:45 +0100

    refactor login flow

--- src/actions/login.js
+++ src/actions/login.js
@@ -8,15 +8,13 @@ const loginSuccessful = user => ({
 
 export function login(user) {
     return dispatch => {
-        console.log(user)
-        fetch("http://35.205.253.173:8080/register", {
+        fetch("http://35.205.253.173:8080/user/register", {
             method: 'post'
         })
         .then(response => console.log(response.text()))
-        .then(text => dispatch(receiveMessage(text)) )
+        .then(text => dispatch(loginSuccessful(user)))
         .catch(() => {
             console.error("Error connecting to the server");
         });
-        dispatch(loginSuccessful(user));
     };
 }
