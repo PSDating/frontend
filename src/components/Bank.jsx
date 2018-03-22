5faf7ff032c8b8f7bb085d72109061ac699aec34
René Bulsing <rene.bulsing@ing.nl>
2018-03-22 11:53:28 +0100

    Adjusted the styling of the Bank and Scanner screen.

--- src/components/Bank.jsx
+++ src/components/Bank.jsx
@@ -2,86 +2,87 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Container, Button, Grid, Input, Dropdown, Icon } from "semantic-ui-react";
 import {
-    BrowserRouter as Router,
-    Route,
-    Link,
-    Redirect
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect
 } from "react-router-dom";
 import logoImage from "../../img/logo.png";
 import rabobankCodeImage from "../../img/shadow-rabobank-code.png";
 class Bank extends React.Component {
 
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            bankName: "",
-            bankAccountNumber: "",
-            bankCardNumber: ""
-        };
+    this.state = {
+      bankName: "",
+      bankAccountNumber: "",
+      bankCardNumber: ""
+    };
 
-        this.updateBank = this.updateBank.bind(this)
-        this.updateBankNumber = this.updateBankNumber.bind(this)
-        this.updateCardNumber = this.updateCardNumber.bind(this)
+    this.updateBank = this.updateBank.bind(this)
+    this.updateBankNumber = this.updateBankNumber.bind(this)
+    this.updateCardNumber = this.updateCardNumber.bind(this)
 
-        this.authorizeAccount = this.authorizeAccount.bind(this);
+    this.authorizeAccount = this.authorizeAccount.bind(this);
+  }
+
+  updateBank(event, data) {
+    this.setState({ bankName: data.value })
+  }
+
+  updateBankNumber(event) {
+    this.setState({ bankAccountNumber: event.target.value })
+  }
+
+  updateCardNumber(event) {
+    this.setState({ bankCardNumber: event.target.value })
+  }
+
+  authorizeAccount(event) {
+    this.props.authorizeAccount(this.state);
+    event.preventDefault();
+  }
+
+  render() {
+    if (this.props.bank) {
+      return (
+        <Redirect
+          to={{
+            pathname: "/scanner"
+          }} />
+      );
     }
-
-    updateBank(event, data) {
-        this.setState({ bankName: data.value })
-    }
-
-    updateBankNumber(event) {
-        this.setState({ bankAccountNumber: event.target.value })
-    }
-
-    updateCardNumber(event) {
-        this.setState({ bankCardNumber: event.target.value })
-    }
-
-    authorizeAccount(event) {
-        console.log(this.state);
-        this.props.authorizeAccount(this.state);
-        event.preventDefault();
-    }
-
-    render() {
-        if (this.props.bank) {
-            return (
-                <Redirect
-                    to={{
-                        pathname: "/scanner"
-                    }} />
-            );
-        }
-        return <div>
-            <img className="logo-intro logo-register logo-bank" src={logoImage} />
-            <Grid
-                style={{ height: '100%' }}
-                textAlign='center'
-            >
-                <Grid.Column style={{ maxWidth: 650 }}>
-                    <form className="ui form" onSubmit={this.handleSubmit}>
-                        <div className="field">
-                            <Dropdown placeholder='Choose your bank...' value={this.state.bankName} onChange={this.updateBank} search selection options={[{ key: 'RB', value: 'RABO', text: 'Rabobank' }]} />
-                        </div>
-                        <div className="field">
-                            <Input icon="credit card" iconPosition="left" value={this.state.bankAccountNumber} onChange={this.updateBankNumber} placeholder='Banknumber' />
-                        </div>
-                        <div className="field">
-                            <Input icon="id card outline" iconPosition="left" value={this.state.bankCardNumber} onChange={this.updateCardNumber} placeholder='Cardnumber' />
-                        </div>
-                        <Button className="fs-button bank-register-button" color='red' onClick={this.authorizeAccount}>Authenticate</Button>
-                    </form>
-                </Grid.Column>`
-            </Grid>
-            <img className={this.state.bankName == 'RABO' ? 'shadow-rabobank-image' : 'hidden'} src={rabobankCodeImage} />
-        </div>
-    }
+    return <div>
+      <img className="logo-intro logo-register logo-bank" src={logoImage} />
+      <Grid
+        style={{ height: '100%' }}
+        textAlign='center'
+      >
+        <Grid.Column style={{ maxWidth: 650 }}>
+          <form className="ui form">
+            <div className="field">
+              <Dropdown placeholder='Choose your bank...' value={this.state.bankName} onChange={this.updateBank} search selection options={[{ key: 'RB', value: 'RABO', text: 'Rabobank' }]} />
+            </div>
+            <div className="field">
+              <Input icon="credit card" iconPosition="left" value={this.state.bankAccountNumber} onChange={this.updateBankNumber} placeholder='Banknumber' />
+            </div>
+            <div className="field">
+              <Input icon="id card outline" iconPosition="left" value={this.state.bankCardNumber} onChange={this.updateCardNumber} placeholder='Cardnumber' />
+            </div>
+            <div className="field">
+              <img className={this.state.bankName == 'RABO' ? 'shadow-rabobank-image' : 'hidden'} src={rabobankCodeImage} />
+            </div>
+            <Button className="fs-button bank-register-button" color='red' onClick={this.authorizeAccount}>Authenticate</Button>
+          </form>
+        </Grid.Column>
+      </Grid>
+    </div>
+  }
 }
 
 Bank.propTypes = {
-    user: PropTypes.object
+  user: PropTypes.object
 };
 
 export default Bank;
--- src/components/Scanner.jsx
+++ src/components/Scanner.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Container, Header, Input, Button, Image, Label } from "semantic-ui-react";
+import { Container, Header, Input, Button, Image, Label, Grid } from "semantic-ui-react";
 import { Redirect } from "react-router-dom";
 import logoImage from "../../img/logo.png";
 
-const Scanner = ({image, authorized, candidates, requestScannerAuthorize, requestRecommendations}) => {
+const Scanner = ({ image, authorized, candidates, requestScannerAuthorize, requestRecommendations }) => {
   const shouldToGoSelection = authorized && candidates.length !== 0;
   if (shouldToGoSelection) {
     return (
@@ -18,15 +18,26 @@ const Scanner = ({image, authorized, candidates, requestScannerAuthorize, reques
   return (
     <div>
       <img className="logo-intro logo-bank" src={logoImage} />
-      <Image className="shadow-rabobank-image" src={image}></Image>
-      <div className="rabobank-code-input">
-      <Input className="full-width" icon="hashtag" iconPosition="left" placeholder='Password' type="password" />
-      </div>
-      <Button className="fs-button bank-scanner-button" color='red' onClick={e => {
-        e.preventDefault();
-        requestScannerAuthorize();
-        requestRecommendations();
-      }}>Authorize</Button>
+      <Grid
+        style={{ height: '100%' }}
+        textAlign='center'
+      >
+        <Grid.Column style={{ maxWidth: 650 }}>
+          <form className="ui form">
+            <div className="field">
+              <Input icon="hashtag" iconPosition="left" placeholder='Password' type="password" />
+            </div>
+            <div className="field">
+              <Image className="shadow-rabobank-image" src={image}></Image>
+            </div>
+            <Button className="fs-button bank-scanner-button" color='red' onClick={e => {
+              e.preventDefault();
+              requestScannerAuthorize();
+              requestRecommendations();
+            }}>Authorize</Button>
+          </form>
+        </Grid.Column>
+      </Grid>
     </div>
   )
 }
